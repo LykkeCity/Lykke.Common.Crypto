8141a88757c3f9b5daba8de65cbf17a46cd97e17
Anton Sivov <Anton.Sivov@inbox.ru>
2017-11-20 23:55:03 +0300

    Added test for checking sign

--- test/Lykke.Common.Crypto.Test/StringCryptoExtTest.cs
+++ test/Lykke.Common.Crypto.Test/StringCryptoExtTest.cs
@@ -5,14 +5,26 @@ namespace Lykke.Common.Crypto.Test
 {
     public class StringCryptoExtTest
     {
+        private static string test = "Test string test string";
+        private static string key = "TestKey";
+
         [Fact]
         public void EqualAlgorithm()
         {
-            string test = "testString test String";
-            string key = "testKey";
+           
             var t = test.AesEncript(key);
             var tt = t.AesDecript(key);
             Assert.Equal(test, test.AesEncript(key).AesDecript(key));
+        }
+
+        [Fact]
+        public void TestEncriptAlorithm()
+        {
+
+            var t = test.AesEncript(key);
+            Assert.Equal("0b4929ccad4ab59a1d0b3efb7bad03bf6464e7b993bdd228d71e41299ad95936820dd2d6d3bd3edda9d654f7300080b9820dd2d6d3bd3edda9d654f7300080b9", t);
+
+ 
         }
     }
 }
